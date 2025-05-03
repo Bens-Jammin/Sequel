68940440766a9b38eabae2d3481a24b5a75384ed
Ben Jammin <benemiller994@gmail.com>
2025-05-03 15:09:08 -0400

    removed redundant files

--- src/log/mod.rs
+++
@@ -1,5 +0,0 @@
-// This directory will be used to log important details of the database operations.
-// This is to help ensure that operations are being made correctly, and to help debug
-// if / when a system crash occurs.
-
-pub mod log_recorder;
